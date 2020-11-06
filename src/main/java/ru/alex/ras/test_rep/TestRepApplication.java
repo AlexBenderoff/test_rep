43763579a56de038f21f778c31bcf946ef96b29f
AlexBenderoff <Shurikadmin1982@>
2020-11-06 16:32:57 +0500

    Initial project setup

---
+++ src/main/java/ru/alex/ras/test_rep/TestRepApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.ras.test_rep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alex/ras/test_rep/TestRepApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex.ras.test_rep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
